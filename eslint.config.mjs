daf55f54def490f27cadbf3676b5a13da3669e16
Teetras <tanya.prokhorenko.2017@Gmail.com>
2024-12-02 00:51:33 +0300

    hotfix dependency and lib

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,16 +2,24 @@ import globals from "globals";
 import pluginJs from "@eslint/js";
 import tseslint from "typescript-eslint";
 import pluginReact from "eslint-plugin-react";
+import pluginJest from "eslint-plugin-jest";
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
   { files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"] },
-  { languageOptions: { globals: globals.browser } },
+  {
+    languageOptions: {
+      globals: {
+        ...globals.browser,
+        ...globals.jest, 
+      },
+    },
+  },
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
   pluginReact.configs.flat.recommended,
+  pluginJest.configs.recommended, 
   {
     ignores: [".node_modules/*"],
   },
 ];
-
